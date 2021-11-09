555daa72c5f535d8f20a3a454d1e298046468b11
mohd isa <mohdisa233@gmail.com>
2021-11-09 18:52:10 +0530

    first

---
+++ main.js
@@ -0,0 +1,53 @@
+const navbar = document.querySelector("nav");
+const menu = document.querySelector(".menu");
+const menuBtn = document.querySelector(".menu-btn");
+
+menuBtn.addEventListener("click", () =>{
+    navbar.classList.toggle('nav-background');
+    menu.classList.toggle('nav-toggle');
+});
+
+// menu
+
+
+var scroll = new SmoothScroll('a[href*="#"]', {
+    speed: 500,
+    speedAsDuration: true
+});
+// for mssg
+
+
+function sendmail() {
+
+    var name = $('#Name').val();
+    var email = $('#Sender').val();
+    var subject = $('#Subject').val();
+    var message = $('#Message').val();
+
+    // var body = $('#body').val();
+
+    var Body = 'Name: ' + name + '<br>Email: ' + email + '<br>Subject: ' + subject + '<br>Message: ' + message;
+    //console.log(name, phone, email, message);
+
+    Email.send({
+      SecureToken: "fbf31702-bb7f-4a4e-9c1c-4ccf17ee777f",
+      To: 'isa527956@gmail.com',
+      From: "mohdisa233@gmail.com",
+      Subject: "New message on contact from " + name,
+      Body: Body
+    }).then(
+      message => {
+        //console.log (message);
+        if (message == 'OK') {
+          alert('Your mail has been send. Thank you for connecting.');
+        }
+        else {
+          console.error(message);
+          alert('There is error at sending message. ')
+
+        }
+
+      }
+    );
+  }
+  
